c5a59845e8fa7e01cc79878fdaa072bbe2b7265b
Najeebulla <najeebsyed16@gmail.com>
2021-02-08 21:58:10 -0500

    Changing UI and fixing bugs

--- lib/main.dart
+++ lib/main.dart
@@ -8,33 +8,40 @@ import 'package:project_tracker_app/Screens/home.dart';
 import 'package:project_tracker_app/Screens/login.dart';
 import 'package:project_tracker_app/Services/auth.dart';
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
 
 class App extends StatelessWidget {
-  // Create the initialization Future outside of `build`:
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return const Scaffold(body: Center(child: Text("Error"),),);
-        }
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return const Scaffold(
+              body: Center(
+                child: Text("Error"),
+              ),
+            );
+          }
 
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Root();
-        }
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Root();
+          }
 
-        // Otherwise, show something whilst waiting for initialization to complete
-        return const Scaffold(body: Center(child: Text("Loading..."),),);
-      },
+          // Otherwise, show something whilst waiting for initialization to complete
+          return const Scaffold(
+            body: Center(
+              child: Text("Loading..."),
+            ),
+          );
+        },
+      ),
     );
   }
 }
